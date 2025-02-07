e144df8e7ef07cf9e470eff09bc8822424eb4ad7
Anton Medvedev <anton@medv.io>
2025-02-07 12:21:25 +0100

    Fix image preview pixels orientation (fixes #159)

--- image.go
+++ image.go
@@ -37,8 +37,8 @@ func drawImage(path string, width, height int) (string, error) {
 	var buffer bytes.Buffer
 	for y := bounds.Min.Y + 1; y < bounds.Max.Y-1; y += 2 {
 		for x := bounds.Min.X + 1; x < bounds.Max.X-1; x++ {
-			r1, g1, b1, a1 := img.At(x, y).RGBA()
-			r2, g2, b2, a2 := img.At(x, y+1).RGBA()
+			r1, g1, b1, a1 := img.At(x, y+1).RGBA()
+			r2, g2, b2, a2 := img.At(x, y).RGBA()
 
 			// If both pixels are transparent, print a space.
 			if a1 < 6553 && a2 < 6553 {
