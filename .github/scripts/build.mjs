9f6f7a0a14b04d2af7197a7e8353223e4697b974
Anton Medvedev <anton@medv.io>
2025-02-07 14:38:29 +0100

    Move build.mjs

---
+++ .github/scripts/build.mjs
@@ -0,0 +1,34 @@
+$.verbose = true
+
+const bin = 'walk'
+const goos = [
+  'linux',
+  'darwin',
+  'windows',
+]
+const goarch = [
+  'amd64',
+  'arm64',
+]
+
+const name = (GOOS, GOARCH) => `${bin}_${GOOS}_${GOARCH}` + (GOOS === 'windows' ? '.exe' : '')
+
+const resp = await fetch('https://api.github.com/repos/antonmedv/fx/releases/latest')
+const {tag_name: latest} = await resp.json()
+
+await $`go mod download`
+
+await Promise.all(
+  goos.flatMap(GOOS =>
+    goarch.map(GOARCH =>
+      $`GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${name(GOOS, GOARCH)}`)))
+
+await Promise.all(
+  goos.flatMap(GOOS =>
+    goarch.map(GOARCH =>
+      $`gh release upload ${latest} ${name(GOOS, GOARCH)}`)))
+
+await Promise.all(
+  goos.flatMap(GOOS =>
+    goarch.map(GOARCH =>
+      $`rm ${name(GOOS, GOARCH)}`)))
